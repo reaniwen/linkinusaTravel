cddbab3864a10aba45e0673b1ecc816b69d94205
reaniwen <reaniwen@gmail.com>
2016-01-01 09:19:23 -0500

    Initial Commit

---
+++ LinkinusaTravel/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LinkinusaTravel
+//
+//  Created by Rean on 1/1/16.
+//  Copyright © 2016 Rean. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
